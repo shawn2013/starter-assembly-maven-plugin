33b5e4a7e7cb69563e6ebd63fa6fa6dd2d6fee4d
shawn <email_lxc@163.com>
2019-02-01 17:09:35 +0800

    update

--- src/main/resources/META-INF/start.sh
+++ src/main/resources/META-INF/start.sh
@@ -1,20 +1,30 @@
 #!/usr/bin/env bash
 
 server_root=`pwd`
-classpath="$server_root/conf:$server_root/lib"
-
+classpath="$server_root/conf:`echo lib/*.jar | tr ' ' ':'`"
 if test -e ${server_root}/conf/env ; then
     source ${server_root}/conf/env
 fi
 
-#
-# check bundle name
-#
-if [ -n "${BUNDLE_JAR_NAME}" ]; then
- 	CUR_BUNDLE_NAME=${BUNDLE_JAR_NAME}
+if [ -n "${mainclass}" ]; then
+ 	CUR_BUNDLE_NAME=${mainclass}
 else
-	echo "please set up BUNDLE_JAR_NAME in env"
+	echo "please set up mainclass in env"
 	exit
+fi
+
+check(){
+    RUNNING=`ps -F -p $PID|grep $PID|grep $CUR_BUNDLE_NAME`
+    if [ ! -z "$RUNNING" ];
+    then
+        echo "$CUR_BUNDLE_NAME is already running..."
+        exit 1
+    fi
+}
+if [ -f pid ];
+then
+    PID=`cat pid`
+    check
 fi
 
 #export JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote.port=9527 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
@@ -23,7 +33,6 @@ fi
 #export JAVA_OPTS="$JAVA_OPTS -XX:+UseParallelGC -XX:+UseParallelOldGC -XX:ParallelGCThreads=2"
 #export JAVA_OPTS="$JAVA_OPTS -XX:+PrintGCDetails -XX:+PrintGCTimeStamps"
 
-log_name=`date "+%Y%m%d%H%M%S"`
-
-echo "nohup java $JAVA_OPTS -Djava.ext.dirs=${classpath} -jar lib/${CUR_BUNDLE_NAME}  >> log_"${log_name}".log 2>&1 &"
-nohup java $JAVA_OPTS -Djava.ext.dirs=${classpath} -jar lib/${CUR_BUNDLE_NAME} >> log_"${log_name}".log 2>&1 &
+echo "nohup java $JAVA_OPTS -cp ${classpath} ${mainclass} >> nohup.log 2>&1 &"
+nohup java $JAVA_OPTS -cp ${classpath} ${mainclass} >> nohup.log 2>&1 &
+echo $! > pid
