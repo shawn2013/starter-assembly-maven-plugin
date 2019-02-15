69563505506b4699e7a823d963b8a17a81315fe1
shawn <email_lxc@163.com>
2019-02-15 15:54:22 +0800

    修改Stop.sh

--- src/main/resources/META-INF/start.sh
+++ src/main/resources/META-INF/start.sh
@@ -13,26 +13,7 @@ else
 	exit
 fi
 
-check(){
-    RUNNING=`ps -F -p $PID|grep $PID|grep $CUR_BUNDLE_NAME`
-    if [ ! -z "$RUNNING" ];
-    then
-        echo "$CUR_BUNDLE_NAME is already running..."
-        exit 1
-    fi
-}
-if [ -f pid ];
-then
-    PID=`cat pid`
-    check
-fi
-
-#export JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote.port=9527 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
-#export JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.snmp.interface=0.0.0.0 -Dcom.sun.management.snmp.port=12345 -Dcom.sun.management.snmp.acl=false"
-#export JAVA_OPTS="$JAVA_OPTS -server -Xms1024m -Xmx1024m -Xmn448m -Xss256K -XX:MaxPermSize=128m -XX:ReservedCodeCacheSize=64m"
-#export JAVA_OPTS="$JAVA_OPTS -XX:+UseParallelGC -XX:+UseParallelOldGC -XX:ParallelGCThreads=2"
-#export JAVA_OPTS="$JAVA_OPTS -XX:+PrintGCDetails -XX:+PrintGCTimeStamps"
-
-echo "nohup java $JAVA_OPTS -cp ${classpath} ${mainclass} >> nohup.log 2>&1 &"
-nohup java $JAVA_OPTS -cp ${classpath} ${mainclass} >> nohup.log 2>&1 &
+log_name=`date "+%Y%m%d"`
+echo "nohup java $JAVA_OPTS -cp ${classpath} ${mainclass} >> log_"${log_name}".log 2>&1 &"
+nohup java $JAVA_OPTS -cp ${classpath} ${mainclass} >> log_"${log_name}".log 2>&1 &
 echo $! > pid
--- src/main/resources/META-INF/stop.sh
+++ src/main/resources/META-INF/stop.sh
@@ -1,4 +1,26 @@
 #!/usr/bin/env bash
-PIDFILE=pid
-kill `cat $PIDFILE`
-rm -f $PIDFILE
+
+server_root=`pwd`
+if test -e ${server_root}/conf/env ; then
+    source ${server_root}/conf/env
+fi
+
+#
+# check bundle name
+#
+if [ -n "${mainclass}" ]; then
+ 	CUR_BUNDLE_NAME=${mainclass}
+else
+	echo "please set up mainclass in env"
+	exit
+fi
+
+
+if [ -n "$CUR_BUNDLE_NAME" ] ; then
+    PIDS=`ps -ef | grep ${CUR_BUNDLE_NAME} | grep -v ' grep' | awk '{print $2}'`
+    for f in `echo ${PIDS[@]}`; do
+        echo "Find process and pid=["$f"]"
+        kill -9 $f
+        echo "Kill pid=["$f"] done"
+    done
+fi
